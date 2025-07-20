5a6672ce20187d6532e0ba3725b6b0ea40fdb4a1
Michael Bonato <michael.bonato@gmail.com>
2025-07-20 23:36:17 +0200

    clean file

--- script/script.js
+++ script/script.js
@@ -14,22 +14,18 @@ async function fetchImagesData() {
 
 document.addEventListener('DOMContentLoaded', async () => {
   const images = await fetchImagesData();
-  const sliderContainers = document.querySelectorAll('[data-slider]');
-
-  sliderContainers.forEach((container, index) => {
-    const sliderId = container.getAttribute('id') || `slider-${index}`;
-    
-    new Slider({
-      containerSelector: `#${sliderId}`,
-      images: images,
-      config: {
-        autoplay: true,
-        duration: 4000,
-        showArrows: true,
-        showDots: true,
-        animation: 'slide',
-        transition: 'transform 0.6s ease'
-      }
-    });
+  const sliderId = document.getElementById('CS').id
+  
+  new Slider({
+    containerSelector: `#${sliderId}`,
+    images: images,
+    config: {
+      autoplay: true,
+      duration: 4000,
+      showArrows: true,
+      showDots: true,
+      animation: 'slide',
+      transition: 'transform 0.6s ease'
+    }
   });
 });
